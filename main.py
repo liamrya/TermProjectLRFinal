96e7608889c3a3936e01beeb48815b2b9ff3ed2f
liamrya <lpryan@andrew.cmu.edu>
2022-11-17 22:22:59 -0500

    edited main

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-def main():
-    print('hello world')
+import characters
+
+from cmu_112_graphics import *
+
+def appStarted(app):
+    pass
+
+def keyPressed(app, event):
+    pass
+
+def redrawAll(app, canvas):
+    pass
+
+runApp(width=400, height=400)
