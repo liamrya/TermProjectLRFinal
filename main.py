7f36007ac51bd08843953ac06d7433d5d3bc0bd1
liamrya <lpryan@andrew.cmu.edu>
2022-11-30 21:00:28 -0500

    main added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def appStarted(app):
     spritestrip = app.loadImage('img/mainsprite.png')
     app.sprites = spritestrip
     app.gameOver = False
+    app.win = False
     app.cFloor = 157
     #background
     app.width = 1800
@@ -44,7 +45,9 @@ def appStarted(app):
     app.isFalling = False
     app.isRunning = False
     app.marioLocation = [200,120]
+    app.marioBounds = [187,137,204,157]
     app.current = app.marioLocation[1]
+    
 
     #GOOMBA APP ITEMS
     app.goomba_load = spritestrip.crop((100, 1318, 160, 1350))
@@ -57,22 +60,28 @@ def appStarted(app):
     app.barrierLocations = []
     app.gapLocations = []
     app.powerLocations = []
-    app.barriersDict = {}
-    app.gapDict = {}
-    app.powerDict = {}
+    app.barriersDict = []
+    app.gapDict = []
+    app.powerDict = []
+
+    #COLLISION HELP
+    app.blockR = False
     loadTerrain(app)
     createGoombas(app)
-    
-def getCellBounds(app, L):
-    if L == app.barrierLocations:
-        x = app.barriersDict
-    if L == app.gapLocations:
-        x = app.gapDict
-    if L == app.powerLocations:
-        x = app.powerDict
-    for i in range(len(L)):
-        x[L[i]] = [17*(L[i][1]) - app.scrollX, 17*L[i][0], 17*(L[i][1]+1) - app.scrollX, 17*(L[i][0]+1)]
 
+def getCellBounds(app):
+    for i in range(len(app.barrierLocations)):
+        x = app.barriersDict
+        L = app.barrierLocations
+        x.append([17*(L[i][1]), 17*L[i][0], 17*(L[i][1]+1), 17*(L[i][0]+1)])
+    for i in range(len(app.gapLocations)):
+        x = app.gapDict
+        L = app.gapLocations
+        x.append([17*(L[i][1]), 17*L[i][0], 17*(L[i][1]+1), 17*(L[i][0]+1)])
+    for i in range(len(app.powerLocations)):
+        x = app.powerDict
+        L = app.powerLocations
+        x.append([17*(L[i][1]), 17*L[i][0], 17*(L[i][1]+1), 17*(L[i][0]+1)])
 #generating terrain
 def loadTerrain(app):
     board = []
@@ -88,7 +97,7 @@ def loadTerrain(app):
                 board[x].append('sky')
                 backupBoard[x].append('sky')
      
-    board = loadTerrainSolver(board, 9, 20)
+    board = loadTerrainSolver(board, 9, 15)
     if board != None:
         app.terrain = board
     else:
@@ -101,6 +110,8 @@ def loadTerrain(app):
                 app.powerLocations.append((i, j))
             if app.terrain[i][j] == 'skyDraw':
                 app.gapLocations.append((i, j))
+    print(app.terrain)
+    getCellBounds(app)
 
 def loadTerrainSolver(board, curRow, curCol):
     cols = len(board[0])
@@ -109,7 +120,7 @@ def loadTerrainSolver(board, curRow, curCol):
     for i in range(cols-curCol):
         newCol = curCol + i
         if isLegal(board, curRow, newCol):
-            if random.randint(0,8) == 5:
+            if random.randint(0,10) == 5:
                 board[curRow][newCol] = 'brick'
                 board[curRow-1][newCol] = 'brick'
                 result = loadTerrainSolver(board, curRow, newCol+1)
@@ -118,7 +129,7 @@ def loadTerrainSolver(board, curRow, curCol):
                 board[curRow][newCol] = 'sky'
                 board[curRow-1][newCol] = 'sky'
         if isLegalAir(board, curRow, newCol):
-            if random.randint(0,6) == 2:
+            if random.randint(0,4) == 2:
                 x = random.randint(1,3)
                 y = random.randint(1,3)
                 board[curRow-3][newCol-1] = 'brick'
@@ -167,25 +178,23 @@ def createGoombas(app):
         else:
             count = 0
     
-        
-            
-
 def keyPressed(app, event):
-    if (event.key == 'Left'):
-        if app.scrollX > 2:
-            app.scrollX -= 2
-            app.currentMario = app.runningLeft
+    if app.win == False:
+        if (event.key == 'Left'):
+            if app.scrollX > 2:
+                app.scrollX -= 2
+                app.currentMario = app.runningLeft
+                app.isRunning = True
+        if (event.key == 'Right'):
+            app.currentMario = app.runningRight
             app.isRunning = True
-    if (event.key == 'Right'):
-        app.currentMario = app.runningRight
-        app.isRunning = True
-        app.scrollX += 2
+            if app.blockR == False:
+                app.scrollX += 2
+        if (event.key == 'Up' and app.isJumping == False and app.isFalling == False):
+            app.isJumping = True
+            app.isFalling = False
     if (event.key == 'r'):
         runApp(width=600, height=200)
-    if (event.key == 'Up' and app.isJumping == False and app.isFalling == False):
-        app.isJumping = True
-        app.isFalling = False
-
 def keyReleased(app, event):
     if (event.key == 'Right'):
         app.isRunning = False
@@ -194,13 +203,6 @@ def keyReleased(app, event):
 
 #taken from https://www.cs.cmu.edu/~112/notes/notes-animations-part4.html
 def timerFired(app):
-    getCellBounds(app, app.barrierLocations)
-    getCellBounds(app, app.gapLocations)
-    getCellBounds(app, app.powerLocations)
-    print(app.powerDict)
-    print(app.barriersDict)
-    print(app.gapDict)
-    
     app.spriteCounter = (1+app.spriteCounter) % len(app.runningRight)
     if app.goombaX > 80:
         app.goombaMove = False
@@ -218,6 +220,7 @@ def timerFired(app):
         app.isStart = False
     #jumping
     if app.isJumping == True:
+        app.marioBounds = [187,app.marioLocation[1]-20,204,app.marioLocation[1]]
         app.currentMario = app.jumpingMario
         if app.marioLocation[1] < app.cFloor-27:
             app.marioLocation[1] -= 10
@@ -234,6 +237,7 @@ def timerFired(app):
 
     #falling
     if app.isFalling == True:
+        app.marioBounds = [187,app.marioLocation[1]-20,204,app.marioLocation[1]]
         app.cFloor = 157
         if app.currentMario != app.jumpingMario:
                 app.currentMario = app.jumpingMario
@@ -250,22 +254,30 @@ def timerFired(app):
         app.currentMario = app.standingMario
 
     #collisions
-    for location in app.goombaLocations:
-        locationNow = location-app.scrollX-app.goombaX
-        if locationNow > 186 and locationNow < 214 and (app.currentMario == app.standingMario or 
-        app.currentMario == app.runningRight or app.currentMario == app.runningLeft): 
+        #goomba collision
+    for i in range(len(app.goombaLocations)):
+        locationNow = app.goombaLocations[i]-app.scrollX-app.goombaX
+        if locationNow > app.marioBounds[0] and locationNow < app.marioBounds[2] and app.marioBounds[1] < 130 and app.marioBounds[3] > 115:
+            app.goombaLocations[i] = -200
+        if locationNow > app.marioBounds[0] and locationNow < app.marioBounds[2] and app.marioBounds[1] > 115: 
             app.gameOver = True
-
-
+        #brick collision
+    # for i in range(len(app.barriersDict)):
+    #     if app.marioBounds[2] > app.barriersDict[i][0]-app.scrollX-5 and app.marioBounds[2] < app.barriersDict[i][0]-app.scrollX:
+    #         app.blockR = True
+    
+    #win condition
+    if app.marioBounds[2] + app.scrollX > 1380:
+        app.win = True
+    
 def drawBack(app, canvas):
     canvas.create_image(300-app.scrollX, 100, image=ImageTk.PhotoImage(app.back))
     canvas.create_image(900-app.scrollX, 100, image=ImageTk.PhotoImage(app.back))
     canvas.create_image(1500-app.scrollX, 100, image=ImageTk.PhotoImage(app.back))
 
-
 def drawBackground(app, canvas):
     for i in range(len(app.terrain)):
-        for j in range(len(app.terrain[i])):
+        for j in range(0+int(app.scrollX/17), len(app.terrain[i])-70+int(app.scrollX/17)):
             if app.terrain[i][j] == 'brick':
                 canvas.create_image(10+app.tileSize*j-app.scrollX, 10+app.tileSize*i, image=ImageTk.PhotoImage(app.brick))
             if app.terrain[i][j] == 'power':
@@ -292,6 +304,10 @@ def drawGameOver(app, canvas):
     canvas.create_rectangle(0,0,600,200, fill='Black')
     canvas.create_text(300,100,text='Game Over', fill = 'White')
 
+def drawWin(app, canvas):
+    canvas.create_rectangle(0,0,600,200, fill='White')
+    canvas.create_text(300,100,text='You win!', fill = 'Black')
+
 def redrawAll(app, canvas):
     drawBack(app, canvas)
     drawBackground(app, canvas)
@@ -299,6 +315,8 @@ def redrawAll(app, canvas):
     drawGoomba(app, canvas)
     if app.gameOver == True:
         drawGameOver(app, canvas)
+    if app.win == True:
+        drawWin(app, canvas)
 def playMario():
     runApp(width=600, height=200)
 playMario()
